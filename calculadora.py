906f37b9b1d4c69d969776320c077e7540efef46
rafabermudez16 <38160762+rafabermudez16@users.noreply.github.com>
2019-08-27 23:12:38 -0500

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+def  add ( nume1 , nume2 ):
+    return nume1 + nume2
+
+def  substract ( nume1 , nume2 ):
+    return nume1 - nume2
+
+def  multiply ( nume1 , nume2 ):
+    return nume1 * nume2
+
+def  divide ( nume1 , nume2 ):
+    return nume1 / nume2
+
+print (add (  8, 9 ))
+print (substract ( 5 , 9 ))
+print (multiply ( -3 , 2))
+print (divide ( 5 , 1))
