2bc3fc5fc9c8601315ec8ec8cbe5c60a7ebb84d8
rafabermudez16 <38160762+rafabermudez16@users.noreply.github.com>
2019-09-09 07:44:34 -0500

    factorial

---
+++ factorial.js
@@ -0,0 +1,19 @@
+const fact=0, aux=0, n=0;
+
+function factorial(n){
+  if(n==0||n==1){
+    return 1
+  }
+}
+if(n<0){
+  console.error("n debe ser mayor a 0")
+}
+if(n>10){
+  console.error("n debe ser menor a 10")
+}
+console.log(0, factorial(0))
+console.log(1, factorial(1))
+console.log(2, factorial(2))
+console.log(3, factorial(3))
+console.log(4, factorial(4))
+console.log(5, factorial(5))
